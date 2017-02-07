8835b2fc0ba0ed9081e6294eed27b9de392f1d5a
MartinCrane <martin.crane@gmail.com>
2017-02-07 16:07:32 -0500

    MC1 with name test

--- bin/run.rb
+++ bin/run.rb
@@ -5,4 +5,17 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
-show_character_movies(character)
+
+while !name_existence_test(character)
+	puts "Name not included!!! Enter another: "
+	character = get_character_from_user
+end
+
+runner(character)
+# film_urls = get_film_urls(character)
+# while film_urls == nil
+# 	puts "character doesnt exist"
+# 	character = get_character_from_user
+# 	characters_exist = get_film_urls(character)
+# end
+
